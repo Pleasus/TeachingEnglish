801c1b719c981d46905c0807fb4ad6025830c128
Keri Schreckengost <keris@live.com>
2017-10-16 21:06:06 -0600

    Flashcards added

--- app/api/flashcards.js
+++ app/api/flashcards.js
@@ -1,7 +1,27 @@
 var flashcards = [
 	{
-		image: "./public/images/apple.jpg",
+		image: "../images/apple1.jpg",
 		solution: "Apple"
+	},
+	{
+		image: "../images/pear1.jpg",
+		solution: "Pear"
+	},
+	{
+		image: "../images/banana1.jpg",
+		solution: "Banana"
+	},
+	{
+		image: "../images/orange1.jpg",
+		solution: "Orange"
+	},
+	{
+		image: "../images/grapes1.jpg",
+		solution: "Grapes"
+	},
+	{
+		image: "../images/strawberry1.jpg",
+		solution: "Strawberry"
 	}
 ];
 
