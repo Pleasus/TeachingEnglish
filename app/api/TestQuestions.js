8c61829813f55f287869854914c427841ba9b569
hnguy0221 <hnguy0221@gmail.com>
2017-10-17 18:52:26 -0600

    Cleaned up code

--- app/api/TestQuestions.js
+++ app/api/TestQuestions.js
@@ -6,19 +6,19 @@ var TestQuestions =
     },
     {
         question: "You said you were with Trevor but that was a ____________! You were with that terrible girl again.",
-        answers: ["dishonest", "insincere", "lie", "false", "C"]
+        answers: ["dishonest", "insincere", "lie", "false"]
     },
     {
         question: "I hope you have finished ____________ your suitcases - the taxi has just arrived!",
-        answers: ["making", "having", "packing", "wrapping", "C"]
+        answers: ["making", "having", "packing", "wrapping"]
     },
     {
         question: "I didn't sleep very well at all last night. It was too cold; I will put another ____________ on the bed tonight.",
-        answers: ["blanket", "pillow", "sheet", "headboard", "A"]
+        answers: ["blanket", "pillow", "sheet", "headboard"]
     },
     {
         question: "The boat sank when it hit a ____________ that was hidden under the water. Everyone had to swim to the beach.",
-        answers: ["rock", "cliff", "coast", "wave", "A"]
+        answers: ["rock", "cliff", "coast", "wave"]
     },
 ];
 
