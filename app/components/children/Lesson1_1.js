3a90e67f85ecf6c891cf3c0f48f4a3cf72a375ed
Keri Schreckengost <keris@live.com>
2017-10-18 20:20:59 -0600

    Flashcards hide solution

--- app/components/children/Lesson1_1.js
+++ app/components/children/Lesson1_1.js
@@ -1,25 +1,33 @@
-// var React = require("react");
-// var flashcards = require("../../api/flashcards");
-// var FlashcardSet = require("./grandchildren/Flashcard_Set");
+var React = require("react");
+var flashcards = require("../../api/flashcards");
+var FlashcardSet = require("./grandchildren/Flashcard_Set");
 
 
-// var Lesson1_1 = React.createClass({
+var Lesson1_1 = React.createClass({
+
+	getInitialState: function () {
+		return{
+			showBtn: "",
+			flashcardSets: flashcards
+		};
+	},
+
+	getShowValue: function(showButton){
+		this.setState({showBtn:showButton})
+	},
+
+	render: function() {
+		return(
+			<div className="container-fluid">
+				<FlashcardSet 
+				flashcardSets={this.state.flashcardSets}
+				getShowValue={this.getShowValue}
+				/>
+			</div>
+		);
+	}
+	
+});
 
 
-// 	render: function() {
-// 		var flashcardSets = [[flashcards[0], flashcards[1]],[flashcards[2], flashcards[3]],[flashcards[4], flashcards[5]]];
-// 		return(
-// 			<div className="container-fluid">
-// 				{flashcardSets.map(function(element,i) {
-// 				 		return (
-// 							<FlashcardSet flashcardSet={element}/>
-// 						);
-// 				 	})
-// 				}
-// 			</div>
-// 		);
-// 	}
-// });
-
-
-// module.exports = Lesson1_1;
+module.exports = Lesson1_1;
--- app/components/children/grandchildren/Flashcard_Set.js
+++ app/components/children/grandchildren/Flashcard_Set.js
@@ -1,36 +1,42 @@
-/*var React = require("react");
+var React = require("react");
 
 var Flashcard_Set = React.createClass({
 
+	showHideBtn: function(event) {
+		var showButton = event.target.value;
+		this.props.getShowValue(showButton);
+	},
+
 	render: function() {
+		var self=this;
+		var divStyle={
+			visibility: "hidden"
+		};
 		return(
 			<div className="row">
-				{this.props.flashcardSet.map(function(element,i) {
-				 		return (
-							<div className="col col-md-6">
+				{this.props.flashcardSets.map((function(element,i) {
+				 	return (
+						<div className="col col-md-6">
 
-								<div>
-									<img src={element.image} width="350px" height="350px" className="img-thumbnail"/>
-								</div>
-
-								<div>
-									function(showHide)(
-										console.log("showHide Working"));
-									<button type="button" className="btn btn-primary btn-sm btn-block" value="Show" onclick=showHide>Show/Hide Definition</button>
-								</div>
-									click function here
-								<div input className="form-control form-control-lg" type="text">
-									{element.solution}
-								</div>
-
+							<div>
+								<img src={element.image} width="350px" height="350px" className="img-thumbnail"/>
 							</div>
-						);
-				 	})
-				}
+
+							<div>
+								<button type="button" className="btn btn-success btn-sm btn-block" value="Show" onClick={self.showHideBtn}>Show/Hide Definition</button>
+							</div>
+
+							<div input className="form-control form-control-lg" type="text" style={divStyle}>
+								{element.solution}
+							</div>
+						</div>
+
+					);
+				}).bind(this))}
 			</div>
 		);
 	}
 });
 
 
-module.exports = Flashcard_Set;*/
+module.exports = Flashcard_Set;
