af0afd6a2291ff1d5e7b53dc4d0df49a78206598
dogray7722 <david.gray61@du.edu>
2017-10-21 06:53:00 -0600

    Move 1_1 to 1_2

--- app/components/children/Lesson1_1.js
+++ app/components/children/Lesson1_1.js
@@ -1,68 +1,23 @@
 var React = require("react");
-var flashcards = require("../../api/flashcards");
-var GreatGrandFlashcards = require("./grandchildren/greatGrandChildren/GreatGrandFlashcards.js");
 
 var Lesson1_1 = React.createClass({
-
-	getInitialState: function() {
-		return {
-			flashcardSets: flashcards,
-			showResults: false,
-			solution: "",
-		};
-	},
-
-	showHideBtn: function(element) {
-		var el = element;
-		console.log(el);
-		this.setState({showResults: true});
-		this.setState({solution: el.solution});
-	},
-
-    displaySolution: function()
+	render: function()
 	{
 		return (
-	        <GreatGrandFlashcards
-		        solution={this.state.solution}
-            />
-		);
-	},
+			<div className="tenses">
+				<h1>1.1 Vocabulary Builder</h1>
+				<h2>Learn Words For:</h2>
+				<h4>Fruit</h4>
+				<h4>Numbers</h4>
+				<h4>Travel</h4>
 
-    componentDidUpdate:function()
-	{
-	},
-
-	render: function() {
-
-		return(
-			<div className="row">
-				{this.state.flashcardSets.map((function(element,i) {
-
-				 	return (
-						<div key={i+1} className="col-md-6">
-                            <div className="row">
-							    <div>
-								    <img src={element.image} width="300px" height="300px"
-								    className="img-thumbnail"/>
-							    </div>
-
-							    <div>
-								    <button
-								        className="flashcardButton btn btn-success"
-								    	onClick={(function() {
-										    this.showHideBtn(element);
-									    }).bind(this)}>Show
-								    </button>
-							    </div>
-                                {this.state.showResults && this.state.solution === element.solution ? this.displaySolution() : ""}
-							</div>
-						</div>
-					);
-				}).bind(this))}
 			</div>
-		);
+			);
 	}
 });
 
-
 module.exports = Lesson1_1;
+
+
+
+
--- app/components/children/Lesson2_1.js
+++ app/components/children/Lesson2_1.js
@@ -1,16 +1,68 @@
 var React = require("react");
+var flashcards = require("../../api/flashcards");
+var GreatGrandFlashcards = require("./grandchildren/greatGrandChildren/GreatGrandFlashcards.js");
 
 var Lesson2_1 = React.createClass({
-	render: function()
+
+	getInitialState: function() {
+		return {
+			flashcardSets: flashcards,
+			showResults: false,
+			solution: "",
+		};
+	},
+
+	showHideBtn: function(element) {
+		var el = element;
+		console.log(el);
+		this.setState({showResults: true});
+		this.setState({solution: el.solution});
+	},
+
+    displaySolution: function()
 	{
 		return (
-			<div>
+	        <GreatGrandFlashcards
+		        solution={this.state.solution}
+            />
+		);
+	},
 
-				<p>This is lesson 2 level 1</p>
+    componentDidUpdate:function()
+	{
+	},
 
+	render: function() {
+
+		return(
+			<div className="row">
+				{this.state.flashcardSets.map((function(element,i) {
+
+				 	return (
+						<div key={i+1} className="col-md-6">
+                            <div className="row">
+							    <div>
+								    <img src={element.image} width="300px" height="300px"
+								    className="img-thumbnail"/>
+							    </div>
+
+							    <div>
+								    <button
+								        className="flashcardButton btn btn-success"
+								    	onClick={(function() {
+										    this.showHideBtn(element);
+									    }).bind(this)}>Show
+								    </button>
+							    </div>
+                                {this.state.showResults && this.state.solution === element.solution ? this.displaySolution() : ""}
+							</div>
+						</div>
+					);
+				}).bind(this))}
 			</div>
-			);
+		);
 	}
 });
+
 
 module.exports = Lesson2_1;
